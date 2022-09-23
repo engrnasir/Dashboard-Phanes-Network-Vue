5cbfc225a23795126b5f4d2c352876bb7c0f6f5a
Nasir Raza <iinasirraza@gmail.com>
2022-09-24 04:54:55 +0500

    updated the changes except the input field

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import router from './router'
 import store from './store'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faFacebookSquare, faTwitterSquare, faDiscord, faTelegram} from '@fortawesome/free-brands-svg-icons'
-import { faBullhorn } from "@fortawesome/free-solid-svg-icons";
+import { faBullhorn, faBars} from "@fortawesome/free-solid-svg-icons";
 library.add(faFacebookSquare);
 library.add(faTwitterSquare);
 library.add(faDiscord);
 library.add(faTelegram);
 library.add(faBullhorn);
+library.add(faBars);
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
